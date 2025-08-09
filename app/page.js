abdb908a1118d33a418eabbf39cddd56b4333af3
Brissou <brice.eliasse@gmail.com>
2025-08-09 22:29:28 +0200

    render

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import useSWR from "swr";
 import fetcher from "@/lib/fetcher";
 import Image from "next/image";
 import { useState, useEffect, useMemo, useRef } from "react";
+import { CometCard } from "@/components/ui/comet-card";
 
 export default function Home() {
   const [data, setData] = useState({
@@ -50,25 +51,74 @@ export default function Home() {
       </div> */}
       <div className="grid grid-cols-12 gap-4 mt-6">
         <div className="col-span-7 bg-black h-full flex items-center justify-center">
-          <div className="relative ring-4 ring-white rounded">
-            <img
-              src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${data.champion.id}_${data.skin.num}.jpg`}
-              alt={data.avatar.name}
-              className="w-[308px] h-[560px]"
-            />
-            <Image
-              src={`/assets/layout.png`}
-              alt="layout"
-              width={640}
-              height={263}
-              className="absolute bottom-[8%] left-1/2 z-10 w-[92%] -translate-x-1/2"
-            />
-            <div className="absolute bottom-[20%] left-0 w-full h-[200px] pointer-events-none bg-gradient-to-t from-black to-transparent"></div>
-            <div className="absolute bottom-0 left-0 w-full h-[21%] pointer-events-none bg-black"></div>
-            <span className="absolute bottom-[5%] left-1/2 -translate-x-1/2 text-xl text-white z-10">
-              {data.summonerName}
-            </span>
-          </div>
+          <CometCard className=" ">
+            <div className="relative ring-4 ring-white rounded">
+              <img
+                src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${data.champion.id}_${data.skin.num}.jpg`}
+                alt={data.avatar.name}
+                className="w-[308px] h-[560px]"
+              />
+              <Image
+                src={`/assets/layout.png`}
+                alt="layout"
+                width={640}
+                height={263}
+                className="absolute bottom-[7%] left-1/2 z-10 w-[92%] -translate-x-1/2"
+              />
+
+              <span className="absolute bottom-[23%] left-1/2 -translate-x-1/2 text-xl text-white z-10 tracking-wide font-medium text-center w-10/12">
+                {data.skin.name === "default"
+                  ? data.champion.name
+                  : data.skin.name}
+              </span>
+
+              <img
+                src={`https://ddragon.leagueoflegends.com/cdn/${latestVersion}/img/profileicon/${data.avatar.id}.png`}
+                alt="Profile icon"
+                width={128}
+                height={128}
+                className="absolute bottom-[11.2%] left-[49.55%] z-10 w-[17.5%] -translate-x-1/2 rounded-full"
+              />
+
+              <img
+                src={`https://ddragon.leagueoflegends.com/cdn/${latestVersion}/img/spell/${data.summonerSpell.d}.png`}
+                alt="D summoner spell"
+                width={40}
+                height={40}
+                className="absolute  bottom-[13.4%] left-[73.55%] z-10 w-[9.3%] -translate-x-1/2 "
+              />
+
+              <img
+                src={`https://ddragon.leagueoflegends.com/cdn/${latestVersion}/img/spell/${data.summonerSpell.f}.png`}
+                alt="F summoner spell"
+                width={40}
+                height={40}
+                className="absolute bottom-[13.4%] left-[85.6%] z-10 w-[9.3%] -translate-x-1/2 "
+              />
+
+              <img
+                src={`https://ddragon.leagueoflegends.com/cdn/img/${data.runes.primary.icon}`}
+                alt="Primary rune"
+                width={50}
+                height={50}
+                className="absolute bottom-[12.9%] left-[15.4%] z-10 w-[10.3%] -translate-x-1/2 "
+              />
+
+              <img
+                src={`https://ddragon.leagueoflegends.com/cdn/img/${data.runes.secondary.icon}`}
+                alt="Secondary rune"
+                width={50}
+                height={50}
+                className="absolute bottom-[12.9%] left-[23.5%] z-10 w-[6.5%] -translate-x-1/2 "
+              />
+
+              <div className="absolute bottom-[20%] left-0 w-full h-[200px] pointer-events-none bg-gradient-to-t from-black to-transparent"></div>
+              <div className="absolute bottom-0 left-0 w-full h-[21%] pointer-events-none bg-black"></div>
+              <span className="absolute bottom-[5%] left-1/2 -translate-x-1/2 text-xl text-white z-10 text-center w-10/12">
+                {data.summonerName}
+              </span>
+            </div>
+          </CometCard>
         </div>
         <div className="col-span-5">
           <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4 max-h-[94vh] overflow-y-auto">
